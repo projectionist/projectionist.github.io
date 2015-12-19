3cedfdd2ed8c0de042257fd1dd3992ed3f38fa18
Sam Taylor <sjltaylor@gmail.com>
2015-12-19 12:03:54 +0000

    full screen video background on landing page

---
+++ js/projectionist.landing.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+  projectionist.landing.init();
+});
+
+projectionist.landing = {
+  init: function (video_element_id) {
+
+    var video = videojs('landing_video', {
+      "controls": false,
+      "loadingSpinner": false,
+      "autoplay": false,
+      "loop": true,
+      "preload": "auto"
+    });
+
+    video.ready(function () {
+      video.play();
+    });
+
+    video.on("playing", function () {
+      console.warn("playing");
+    });
+
+    this.trackWindowSize(video);
+  },
+  trackWindowSize: function (video) {
+
+    var resize = function () {
+      var videoElement = $(video.el()).find('video')[0];
+      if (!(videoElement instanceof HTMLVideoElement)) return;
+
+      videoElement.width = Math.max(window.outerWidth, 800);
+    }
+
+    // avoid issues with resize event timing
+    var delayedDoubleResize = function () {
+      resize();
+      setTimeout(resize, 650);
+    }
+
+    resize();
+    $(window).resize(delayedDoubleResize);
+  }
+}
