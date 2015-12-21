109674d746c4b3fa303121ecf734fd52808d5370
Sam Taylor <sjltaylor@gmail.com>
2015-12-21 11:36:47 +0000

    wip landing text

--- js/projectionist/landing.js
+++ js/projectionist/landing.js
@@ -8,47 +8,30 @@ projectionist.landing = {
     var uaParser = new UAParser();
 
     if (uaParser.getOS().name === "iOS") {
-      $('#landing_video').remove();
-    } else {
-      projectionist.landing.video();
+      this.initIOS();
+      return;
     }
+
+    projectionist.landing.video();
+  },
+  initIOS: function () {
+    $('#landing_video').remove();
+    $('.overlay').show();
   },
   video: function () {
     $('#landing_video').show();
+
     var video = videojs('landing_video', {
       "controls": false,
       "loadingSpinner": false,
-      "autoplay": false,
+      "autoplay": true,
       "loop": true,
       "preload": "auto"
     });
 
-    video.ready(function () {
-      video.play();
-      //projectionist.landing.switchOn();
+    video.on("loadeddata", function () {
+      projectionist.landing.switchOn();
     });
-
-    video.on("playing", function () {
-      console.warn("playing");
-    });
-  },
-  trackBackgroundToWindowSize: function (video) {
-
-    var resize = function () {
-      var videoElement = $(video.el()).find('video')[0];
-      if (!(videoElement instanceof HTMLVideoElement)) return;
-
-      videoElement.width = Math.max(window.outerWidth, 800);
-    }
-
-    // avoid issues with resize event timing
-    var delayedDoubleResize = function () {
-      resize();
-      setTimeout(resize, 650);
-    }
-
-    resize();
-    $(window).resize(delayedDoubleResize);
   },
   switchOn: function () {
     $('.projected-text').addClass('on');
